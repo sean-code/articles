e42571ecf951e71c5f76a727c7b3b1455805a9dc
sean-code <john.nganga@student.moringaschool.com>
2022-10-29 18:39:45 +0300

    Added first deliverables

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article
 
+    attr_accessor :author, :magazine, :title
 
+    def initialize(author:, magazine:, title:)
+        @author = author
+        @magazine = magazine
+        @title = title
+    end
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -2,11 +2,15 @@ class Magazine
   attr_accessor :name, :category
 
 
-  def initialize(name, category)
+  def initialize(name:, category:)
     @name = name
     @category = category
 
   end
 
-
 end
+
+mag = Magazine.new(name: "Shujaaz", category: "Comic")
+
+puts mag.name
+puts mag.category
