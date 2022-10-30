44eb265ea3df0fa71e92961f2ebc9c4d269107f3
sean-code <john.nganga@student.moringaschool.com>
2022-10-30 16:45:42 +0300

    Return an array of all Article instances

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,17 @@ class Article
 
     attr_accessor :author, :magazine, :title
 
+    @@all = []
+
     def initialize(author:, magazine:, title:)
         @author = author
         @magazine = magazine
         @title = title
     end
+
+
+    def self.all 
+        @@all
+    end
+
 end
